12d863247feb65926fc9ea105201a600658c00ef
anton_evmenenko <anton.evmenenko.93@gmail.com>
2015-09-11 04:04:02 +0400

    DCM based IMU

--- src/main.c
+++ src/main.c
@@ -30,16 +30,22 @@ unsigned long ms_from_start = 0; // time from start in milliseconds
 
 float angle_x = 0, previous_angle_x = 0, angle_y = 0, previous_angle_y = 0;
 unsigned long previous_timestamp_us = 0, dt = 0, previous_dt = 0;
-int16_t gyro_x = 0, gyro_y = 0, gyro_z = 0;
-int16_t accel_x = 0, accel_y = 0, accel_z = 0;
-int16_t compass_x = 0, compass_y = 0, compass_z = 0;
+vector3 gyroscope = { 0., 0., 0. };
+vector3 accelerometer = { 0., 0., 0. };
+vector3 compass = { 0., 0., 0. };
 int16_t calibration_counter = calibration_iterations_count;
 uint8_t calibration = 1;
 uint16_t bad_values_counter = bad_values_iterations_count;
 int32_t gyro_x_sum = 0, gyro_y_sum = 0, gyro_z_sum = 0;
-float gyro_x_correction = 0, gyro_y_correction = 0, gyro_z_correction = 0;
+vector3 gyroscope_correction = { 0., 0., 0. };
 unsigned long g_squared_sum = 0;
 float g_squared_average = 0;
+
+matrix3f DCM = { { 1., 0., 0., },
+                 { 0., 1., 0., },
+                 { 0., 0., 1., } };
+
+vector3f Euler_angles = { 0., 0., 0. };
 
 void SysTick_init( void )
 {
@@ -95,31 +101,35 @@ int main(void)
 	
     while( 1 )
     {		 
-        getGyroValues( &gyro_x, &gyro_y, &gyro_z );
-        getAccelValues( &accel_x, &accel_y, &accel_z );
-        getRawCompassValues( &compass_x, &compass_y, &compass_z );
+        getGyroValues( gyroscope, gyroscope + 1, gyroscope + 2 );
+        getAccelValues( accelerometer, accelerometer + 1, accelerometer + 2 );
+        getRawCompassValues( compass, compass + 1, compass + 2 );
 
         if ( ( bad_values_counter ? bad_values_counter-- : 0 ) > 0 ) {
             continue;
         }
 
         if ( calibration ) {
-            gyro_x_sum += gyro_x;
-            gyro_y_sum += gyro_y;
-            gyro_z_sum += gyro_z;
+            gyro_x_sum += gyroscope[ 0 ];
+            gyro_y_sum += gyroscope[ 1 ];
+            gyro_z_sum += gyroscope[ 2 ];
 
-            g_squared_sum += sqr( accel_x ) + sqr( accel_y ) + sqr( accel_z );
+            g_squared_sum += vector3_length_squared( accelerometer );
 
             if ( !( calibration_counter ? calibration_counter-- : 0 ) ) {
                 calibration = 0;
 
-                gyro_x_correction = gyro_x_sum / (float)calibration_iterations_count;
-                gyro_y_correction = gyro_y_sum / (float)calibration_iterations_count;
-                gyro_z_correction = gyro_z_sum / (float)calibration_iterations_count;
+                gyroscope_correction[ 0 ] = gyro_x_sum / (float)calibration_iterations_count;
+                gyroscope_correction[ 1 ] = gyro_y_sum / (float)calibration_iterations_count;
+                gyroscope_correction[ 2 ] = gyro_z_sum / (float)calibration_iterations_count;
 
                 g_squared_average = g_squared_sum / (float)calibration_iterations_count;
             }
         } else {
+            vector3f gyroscope_corrected = { gyroscope[ 0 ] - gyroscope_correction[ 0 ],
+                                             gyroscope[ 1 ] - gyroscope_correction[ 1 ],
+                                             gyroscope[ 2 ] - gyroscope_correction[ 2 ] };
+            
             unsigned long timestamp_us = get_us_from_start( );
             dt = timestamp_us - previous_timestamp_us;
             previous_timestamp_us = timestamp_us;
@@ -128,11 +138,12 @@ int main(void)
                 dt = previous_dt;
             }	
 
-            angle_x += ( k * dt * ( gyro_x - gyro_x_correction ) );
-            angle_y += ( k * dt * ( gyro_y - gyro_y_correction ) );
+            /*
+            angle_x += ( k * dt * gyroscope_corrected[ 0 ] );
+            angle_y += ( k * dt * gyroscope_corrected[ 1 ] );
 
-            int u_x = (int)(angle_x * kp + ( angle_x - previous_angle_x ) * kd * 1000000. / (float)dt );
-            int u_y = (int)(angle_y * kp + ( angle_y - previous_angle_y ) * kd * 1000000. / (float)dt );
+            int u_x = (int)(angle_x * kp + ( angle_x - previous_angle_x ) * kd * 1000000. / ( float )dt );
+            int u_y = (int)(angle_y * kp + ( angle_y - previous_angle_y ) * kd * 1000000. / ( float )dt );
 
             previous_angle_x = angle_x;
             previous_angle_y = angle_y;
@@ -141,6 +152,17 @@ int main(void)
             M2_POWER = range( PWM_MIN_SIGNAL + u_x, PWM_MIN_SIGNAL, PWM_MAX_SIGNAL );
             M3_POWER = range( PWM_MIN_SIGNAL - u_y, PWM_MIN_SIGNAL, PWM_MAX_SIGNAL );
             M4_POWER = range( PWM_MIN_SIGNAL + u_y, PWM_MIN_SIGNAL, PWM_MAX_SIGNAL );
+            */
+            
+            matrix3f rotation_matrix, delta_angle, delta_DCM;
+            vector3f_to_rotation_matrix3f( rotation_matrix, gyroscope_corrected );
+            matrix3f_scalar_multiplication( delta_angle, rotation_matrix, 1000000. / ( float )dt );
+            matrix3f_matrix3f_multiplication( delta_DCM, DCM, delta_angle );
+            matrix3f_matrix3f_sum( DCM, DCM, delta_DCM );
+            
+            // TODO : normalizaton
+            
+            DCM_to_Euler_angles( Euler_angles, Euler_angles + 1, Euler_angles + 2, DCM );
         }
     }
 }
